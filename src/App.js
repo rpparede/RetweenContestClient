6eb34f6fd7af0376b613183ac8e59c7863be85e2
rpparede <rpparede@uwaterloo.ca>
2020-12-06 01:40:20 -0800

    Fetch Api using form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
+import React , {useState, useEffect, useInput} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [winner, setWinner] = useState('');
+  const [tweetId, setTweetId] = useState('');
+  
+  const proxyurl = "https://cors-anywhere.herokuapp.com/";
+  useEffect(() => {
+ 
+  })
+
+  const mySubmitHandler = (event) => {
+    event.preventDefault();
+    fetch(proxyurl + "https://stark-peak-57271.herokuapp.com/twitterContest/"+ tweetId)
+    .then(response => response.json())
+    .then(data => setWinner(data.user.screen_name))
+    .catch( e => {
+      console.log(e);
+    })
+  }
+  const myChangeHandler = (event) => {
+    setTweetId(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <form onSubmit={mySubmitHandler}>
+    <h1>Please enter your Tweet ID below:</h1>
+    <input
+      type='text'
+      onChange={myChangeHandler}
+    />
+    <input
+      type='submit'
+    />
+    </form>
+    <div>
+     { winner !== '' && <h1> Winner is {winner} </h1>}
+     </div>
+     </div>
   );
 }
 
